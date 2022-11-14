2052532dcb56f3d21740bc27d0d41cbb1a763add
Tahlil Chowdhury <tah.fighter@hotmail.co.uk>
2022-11-14 03:18:17 +0000

    added single product page with linked php files

---
+++ index.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
---
+++ singleproduct.php
@@ -0,0 +1,150 @@
+<?php
+    include("response.php");
+    $newObj = new Product();
+    $prods = $newObj->get_bluecircle_multi_cement_25kg();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Categories</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="main.css">
+    <script src="https://kit.fontawesome.com/ce98f0dc47.js" crossorigin="anonymous"></script>
+</head>   
+<body>
+    <div class="container">
+        <div class="nav-wrapper">
+            <div class="menu">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="categories.html">Categories</a> </li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+            <div class="search-box">
+                <input type="text" class="tbox" placeholder="Search..." />
+                <button class="btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+            <div class="sign-in">
+                <a href="signin.html">Sign In/Register</a>
+            </div>
+        </div>
+        <div class="single-product">
+            <div class="row">
+                <div class="col-6">
+                    <div class="product-image">
+                        <div class="product-image-main">
+                            <img src="img/tshirt-1.png" alt="" id="product-main-image">
+                        </div>
+                        <div class="product-image-slider">
+                            <img src="img/tshirt-1.png" alt=""  class="image-list">
+                            <img src="img/tshirt-2.png" alt=""  class="image-list">
+                            <img src="img/tshirt-3.png" alt=""  class="image-list">
+                            <img src="img/tshirt-group.png" alt=""  class="image-list">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="breadcrumb">
+                        <span><a href="#">Home</a></span>
+                        <span><a href="#">Product</a></span>
+                        <span class="active">T-shirt</span>
+                    </div>
+
+                    <div class="product">
+                        <div class="product-title">
+                            <h2>Half Sleve T-shirt for Men</h2>
+                        </div>
+                        <div class="product-rating">
+                            <span><i class="bx bxs-star"></i></span>
+                            <span><i class="bx bxs-star"></i></span>
+                            <span><i class="bx bxs-star"></i></span>
+                            <span><i class="bx bxs-star"></i></span>
+                            <span><i class="bx bxs-star"></i></span>
+                            <span class="review">(47 Review)</span>
+                        </div>
+                        <div class="product-price">
+                            <span class="offer-price">$99.00</span>
+                            <span class="sale-price">$129.00</span>
+                        </div>
+
+                        <div class="product-details">
+                            <h3>Description</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos est magnam quibusdam maiores sit perferendis minima cupiditate iusto earum repudiandae maxime vitae nostrum, ea cumque iste ipsa hic commodi tempore.</p>
+                        </div>
+                        <div class="product-size">
+                            <h4>Size</h4>
+                            <div class="size-layout">
+                                <input type="radio" name="size" value="S" id="1" class="size-input">
+                                <label for="1" class="size">S</label>
+
+                                <input type="radio" name="size" value="M" id="2" class="size-input">
+                                <label for="2" class="size">M</label>
+
+                                <input type="radio" name="size" value="L" id="3" class="size-input">
+                                <label for="3" class="size">L</label>
+
+                                <input type="radio" name="size" value="XL" id="4" class="size-input">
+                                <label for="4" class="size">XL</label>
+
+                                <input type="radio" name="size" value="XXL" id="5" class="size-input">
+                                <label for="5" class="size">XXL</label>
+                            </div>
+                        </div>
+                        <div class="product-color">
+                            <h4>Color</h4>
+                            <div class="color-layout">
+                                <input type="radio" name="color"  value="black" class="color-input">
+                                <label for="black" class="black"></label>
+                                <input type="radio" name="color"  value="red" class="color-input">
+                                <label for="red" class="red"></label>
+
+                                <input type="radio" name="color"  value="blue" class="color-input">
+                                <label for="blue" class="blue"></label>
+                            </div>
+                        </div>
+                        <span class="divider"></span>
+
+                        <div class="product-btn-group">
+                            <div class="button buy-now"><i class='bx bxs-zap' ></i> Buy Now</div>
+                            <div class="button add-cart"><i class='bx bxs-cart' ></i> Add to Cart</div>
+                            <div class="button heart"><i class='bx bxs-heart' ></i> Add to Wishlist</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class = "products">
+            <div class = "container">
+            <table id="product" class="table" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Product Name</th>
+                    <th>Supplier Name</th>
+                    <th>Price (£)</th>
+                  </tr>
+            </thead>               
+            <tbody>
+                <?php foreach($prods as $key =>$prod) :?>
+                <tr>
+                  <td><?php echo $prod['id'] ?></td>
+                  <td><?php echo $prod['product_name'] ?></td>
+                  <td><?php echo $prod['supplier_name'] ?></td>
+                  <td><?php echo $prod['price'] ?></td>
+                </tr>
+               <?php endforeach;?>
+            </table>
+            </div>
+        </div>
+        <script src="script.js"></script>
+
+    </div>  
+</body>
+</html>
