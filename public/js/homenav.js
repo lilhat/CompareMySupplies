77fda444aebec1e13f054a21dac33001ad3dd11c
Tahlil Chowdhury <tah.fighter@hotmail.co.uk>
2022-11-15 03:38:36 +0000

    full revamp

---
+++ public/js/homenav.js
@@ -0,0 +1,40 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+        <div class="nav-wrapper">
+            <div class="main-logo">
+                <a href="index.html">
+                    <img src="images/main-logo.png" class="brand-logo" alt="">
+                </a>
+            </div>
+            <div class="top-menu">
+            <ul class="top-links-container">
+                <li class="top-link-item"><a href="index.html">Home</a></li>
+                <li class="top-link-item"><a href="categories.html">All Categories</a> </li>
+            </ul>
+            </div>
+            <div class="sign-in">
+                <a href="signin.html">
+                    <img src="images/user.png" class="user-pic">
+                <span class="text-below">Sign In</span>
+                </a>
+            </div>
+
+        </div>
+        <div class="menu">
+            <ul class="links-container">
+                <li class="link-item"><a href="index.html">Plaster</a></li>
+                <li class="link-item"><a href="categories.html">Concrete</a> </li>
+                <li class="link-item"><a href="about.html">Cement</a></li>
+                <li class="link-item"><a href="contact.html">Timber</a></li>
+                <li class="link-item"><a href="contact.html">Adhesives</a></li>
+                <li class="link-item"><a href="contact.html">Electricals</a></li>
+            </ul>
+        </div>
+    `;
+
+    
+}
+
+createNav();
