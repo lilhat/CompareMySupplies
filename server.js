982a46c4fab7fbcee2ebc6ad85555be2ae01fd4b
Tahlil Chowdhury <tah.fighter@hotmail.co.uk>
2022-11-29 04:31:06 +0000

    signup form

--- server.js
+++ server.js
@@ -2,17 +2,34 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
-const { restart } = require('nodemon');
-const path = reuqire('path');
+const path = require('path');
 
+// declare static path
+let staticPath = path.join(__dirname, "public");
 
 // init express.js
 const app = express();
 
-//routes
-//home route
+app.use(express.static(staticPath));
+
+// routes
+// home route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(staticPath, "index.html"));
+})
+
+// signup route
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(staticPath, "signup.html"));
+})
+
+// 404 route
+app.get('/404', (req, res) => {
+    res.sendFile(path.join(staticPath, "404.html"));
+})
+
+app.use((req, res) => {
+    res.redirect('/404');
 })
 
 app.listen(3000, () => {
