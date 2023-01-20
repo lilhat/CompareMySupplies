a0cf1637062295dec5ef775dde7a85e64591675e
lilhat <tah.fighter@hotmail.co.uk>
2023-01-20 23:03:24 +0000

    added descriptions

--- webScraper_v2/tpScraper.py
+++ webScraper_v2/tpScraper.py
@@ -6,39 +6,44 @@ import concurrent.futures
 
 # # Loop through each response and scrape the first page of products into product dictionary
 NUM_RETRIES = 5
-NUM_THREADS = 5
+NUM_THREADS = 50
 products = []
 urls = []
 
 
 def get_categories():
-    response = requests.get(
-        url='https://app.scrapingbee.com/api/v1/',
-        params={
-            'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
-            'url': 'https://www.travisperkins.co.uk/',
-        },
+    for _ in range(NUM_RETRIES):
+        response = requests.get(
+            url='https://app.scrapingbee.com/api/v1/',
+            params={
+                'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
+                'url': 'https://www.travisperkins.co.uk/',
+            },
 
-    )
-    body = response.content
-    soup = BeautifulSoup(body, 'html.parser')
-    product_elements = soup.find_all('li',
-                                     {'class': 'NavMenuListDesktop__NavMenuTableCategoryListItem-sc-1ypu5gb-2 gvVYHL'})
-    links = []
-    for product_element in product_elements:
-        link_element = product_element.find("a")
-        if link_element:
-            links.append(link_element['href'])
+        )
+        if response.status_code == 200:
+            body = response.content
+            soup = BeautifulSoup(body, 'html.parser')
+            product_elements = soup.find_all('li',
+                                             {'class': 'NavMenuListDesktop__NavMenuTableCategoryListItem-sc-1ypu5gb-2 gvVYHL'})
+            links = []
+            for product_element in product_elements:
+                link_element = product_element.find("a")
+                if link_element:
+                    links.append(link_element['href'])
 
-    department_links = [link for link in links if '/product' in link]
-    print(department_links)
+            department_links = [link for link in links if '/product' in link]
+            print(department_links)
 
-    # Variable i is the page number
-    i = 1
+            # Variable i is the page number
+            i = 1
 
-    for j in range(0, len(department_links)):
-        url_single = 'https://www.travisperkins.co.uk' + department_links[j]
-        urls.append(url_single)
+            for j in range(0, len(department_links)):
+                url_single = 'https://www.travisperkins.co.uk' + department_links[j]
+                urls.append(url_single)
+            break
+        else:
+            print("Error: " + str(response.status_code))
 
 
 def send_request(url):
@@ -62,6 +67,24 @@ def send_request(url):
 get_categories()
 
 
+def single_request(url):
+    for _ in range(NUM_RETRIES):
+        response = requests.get(
+            url='https://app.scrapingbee.com/api/v1/',
+            params={
+                'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
+                'url': url,
+            },
+
+        )
+        if response.status_code == 200:
+            description = scrape_desc(response.content)
+            return description
+        else:
+            print("Error: " + str(response.status_code))
+            print("Url: " + url)
+
+
 def scrape_url(body):
     try:
         soup = BeautifulSoup(body, 'html.parser')
@@ -81,6 +104,7 @@ def scrape_url(body):
         price_list = []
         link_list = []
         image_list = []
+        desc_list = []
         for product_title in product_titles:
             title_element = product_title.text
             if title_element:
@@ -95,15 +119,35 @@ def scrape_url(body):
                 price_list.append(text)
 
         for product_link in product_links:
-            link_list.append('https://www.travisperkins.co.uk' + product_link['href'])
+            full_url = 'https://www.travisperkins.co.uk' + product_link['href']
+            link_list.append(full_url)
+            desc_list.append(single_request(full_url))
 
         for product_image in product_images:
             image_link = product_image['src']
             image_list.append("https://" + image_link[2:])
 
-        for product, price, link, image in zip(product_list, price_list, link_list, image_list):
-            print({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image})
-            products.append({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image})
+        for product, price, link, image, description in zip(product_list, price_list, link_list, image_list, desc_list):
+            print({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image, 'description': description})
+            products.append({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image, 'description': description})
+
+    except KeyError:
+        print("Key Error occurred")
+    except:
+        print("Error occurred")
+
+
+def scrape_desc(body):
+    try:
+        soup = BeautifulSoup(body, 'html.parser')
+        product_desc = soup.find('div', {'data-test-id': 'product-overview'})
+
+        if product_desc:
+            description = product_desc.text
+        else:
+            description = "N/A"
+
+        return description
 
     except KeyError:
         print("Key Error occurred")
--- webScraper_v2/wickesScraper.py
+++ webScraper_v2/wickesScraper.py
@@ -7,7 +7,7 @@ import concurrent.futures
 
 # Loop through each response and scrape the first page of products into product dictionary
 NUM_RETRIES = 5
-NUM_THREADS = 5
+NUM_THREADS = 50
 products = []
 urls = []
 
@@ -60,6 +60,24 @@ def send_request(url):
 get_categories()
 
 
+def single_request(url):
+    for _ in range(NUM_RETRIES):
+        response = requests.get(
+            url='https://app.scrapingbee.com/api/v1/',
+            params={
+                'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
+                'url': url,
+            },
+
+        )
+        if response.status_code == 200:
+            description = scrape_desc(response.content)
+            return description
+        else:
+            print("Error: " + str(response.status_code))
+            print("Url: " + url)
+
+
 def scrape_url(body):
     try:
         soup = BeautifulSoup(body, 'html.parser')
@@ -77,6 +95,7 @@ def scrape_url(body):
         price_list = []
         link_list = []
         image_list = []
+        desc_list = []
         for product_title in product_titles:
             title_element = product_title.text
             if title_element:
@@ -91,15 +110,44 @@ def scrape_url(body):
                 price_list.append(text)
 
         for product_link in product_links:
-            link_list.append('https://www.wickes.co.uk' + product_link['href'])
+            full_url = 'https://www.wickes.co.uk' + product_link['href']
+            link_list.append(full_url)
+            desc_list.append(single_request(full_url))
 
         for product_image in product_images:
             image_link = product_image['src']
             image_list.append("https://" + image_link[2:])
 
-        for product, price, link, image in zip(product_list, price_list, link_list, image_list):
-            print({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image})
-            products.append({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image})
+        for product, price, link, image, description in zip(product_list, price_list, link_list, image_list, desc_list):
+            print({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image, 'description': description})
+            products.append({'product': product, 'price': price, 'category': category_title, 'link': link, 'image': image, 'description': description})
+
+    except KeyError:
+        print("Key Error occurred")
+    except:
+        print("Error occurred")
+
+
+def scrape_desc(body):
+    try:
+        soup = BeautifulSoup(body, 'html.parser')
+        product_desc = soup.find('div', {'class': 'product-main-info__description'})
+
+        if product_desc:
+            description = re.sub('^[\s]+|[\s]+$', '', product_desc.text)
+        else:
+            description = "N/A"
+
+        product_desc2 = soup.find('div', {'class': 'info-content'})
+
+        if product_desc2:
+            span_tags = product_desc2.find_all('span')
+            for tag in span_tags:
+                description += tag.text + ' '
+        else:
+            return description
+
+        return description
 
     except KeyError:
         print("Key Error occurred")
