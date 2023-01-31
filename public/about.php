2bee69f99b912430078047b8fd29380cf65a5e8a
Tahlil Chowdhury <tah.fighter@hotmail.co.uk>
2023-01-31 23:21:11 +0000

    contact page and layout

---
+++ public/about.php
@@ -0,0 +1,52 @@
+<?php
+include("response.php");
+$newObj = new Product();
+$newObj2 = new Product();
+
+$id = $_GET['product'];
+$prods = $newObj->get_Category_id($id);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo $id?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/products.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ce98f0dc47.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+
+</head>   
+<body>
+    <nav class="navbar"></nav>
+    <div class="container">
+        
+        <div class="content-wrapper">
+            <div class="two-column-wrapper">
+                <div class="profile-image-wrapper">
+                    <img src="images/thumbs.jpg">
+                </div>
+                <div class="profile-content-wrapper">
+                    <h1>Innovating the way you buy materials!</h1>
+                    <b1><strong>The best material comparison website on the internet! Saving you money on every purchase.</strong></b1>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <footer></footer>
+    <script src="js/nav.js"></script>
+    <script src="js/footer.js"></script>
+</body>
+
+</html>
