2c009e29aedada01b18d7eb9bb332e194dbcadb2
Tahlil Chowdhury <tah.fighter@hotmail.co.uk>
2023-02-01 19:44:56 +0000

    added builderdepot

--- webScraper_v2/builderdepotScraper.py
+++ webScraper_v2/builderdepotScraper.py
@@ -22,26 +22,24 @@ def get_categories():
             url='https://app.scrapingbee.com/api/v1/',
             params={
                 'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
-                'url': 'https://www.screwfix.com/c/tools/cat830034',
+                'url': 'https://www.builderdepot.co.uk/',
                 'extract_rules': '{"all_links":{"selector":"a","type":"list","output":"@href"}}',
+                'render_js': 'false',
             },
 
         )
         if response.status_code == 200:
             body = response.text
             content_json = json.loads(body)
-            print(body)
+            # print(body)
             # Access the list from the JSON object
             links = content_json["all_links"]
             links = [item for item in links if item is not None]
-            department_links = [link for link in links if '/c/' in link]
-            department_links = [department_link for department_link in department_links if 'https' in department_link]
+            links = links[6:1724]
+            department_links = [item for item in links if item != '#']
             print(department_links)
 
-            # Variable i is the page number
-            i = 1
-
-            for j in range(0, len(department_links)):
+            for j in range(0, 10):
                 urls.append(department_links[j])
                 print(department_links[j])
             break
@@ -56,6 +54,7 @@ def send_request(url):
             params={
                 'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
                 'url': url,
+                'render_js': 'false',
             },
 
         )
@@ -74,6 +73,7 @@ def single_request(url):
             params={
                 'api_key': 'N25JJBPDKWXCENSFCR66CALWK0CE0QHEUE2H82Y2S1RYM4RQGHC1LTMTCX7DIONSJFYSP2ONBX2L0SRI',
                 'url': url,
+                'render_js': 'false',
             },
 
         )
@@ -88,10 +88,10 @@ def single_request(url):
 def scrape_url(body):
     try:
         soup = BeautifulSoup(body, 'html.parser')
-        product_titles = soup.find_all('div', {'class': 'lii__r'})
-        product_prices = soup.find_all('div', {'class': 'lii_price'})
-        category_title_html = soup.find('h1', {'class': 'h1wrapper__h1-special'})
-        product_images = soup.find_all('img', {'id': 'product_image'})
+        product_titles = soup.find_all('strong', {'class': 'product name product-item-name'})
+        product_prices = soup.find_all('div', {'class': 'price-box price-final_price'})
+        category_title_html = soup.find('h1', {'class': 'page-title'})
+        product_images = soup.find_all('img', {'class': 'product-image-photo responsively-lazy'})
 
         if category_title_html is not None:
             category_title = re.sub('^[\s]+|[\s]+$', '', category_title_html.text)
@@ -103,17 +103,18 @@ def scrape_url(body):
         link_list = []
         image_list = []
         desc_list = []
+
         for i in range(0, len(product_titles)):
-            title_raw = product_titles[i].find('a', {'class': 'fh_product_click'})
+            title = re.sub('^[\s]+|[\s]+$', '', product_titles[i].text)
+            title_raw = product_titles[i].find('a')
             link = title_raw['href']
             link_list.append(link)
             desc_list.append(single_request(link))
-            title_element = re.sub('^[\s]+|[\s]+$', '', title_raw.text)
-            if title_element:
-                product_list.append(title_element)
+            if title:
+                product_list.append(title)
 
         for i in range(0, len(product_prices)):
-            price = product_prices[i].find('h4').text
+            price = re.sub('^[\s]+|[\s]+$', '', product_prices[i].find('span', {'class': 'price'}).text)
             price_list.append(price)
 
         for i in range(0, len(product_images)):
@@ -133,24 +134,16 @@ def scrape_url(body):
     except:
         print("Error occurred")
 
+
 def scrape_desc(body):
     try:
         soup = BeautifulSoup(body, 'html.parser')
-        product_desc = soup.find('p', {'id': 'product_long_description_container'})
-
+        product_desc = soup.find('div', {'class': 'data item content first active'})
         if product_desc:
             description = re.sub('^[\s]+|[\s]+$', '', product_desc.text)
+            description = description.replace('\n', ' ')
         else:
             description = "N/A"
-
-        product_desc2 = soup.find('ul', {'class': 'pr__spec-list'})
-
-        if product_desc2:
-            span_tags = product_desc2.find_all('li')
-            for tag in span_tags:
-                description += tag.text + ' '
-        else:
-            return description
 
         return description
 
@@ -167,4 +160,4 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
 
 # Write products into csv file
 df = pd.DataFrame(products)
-df.to_csv('screwfixProducts.csv', index=False)
+df.to_csv('builderdepotProducts.csv', index=False)
