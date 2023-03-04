609f034f40ebee2288ee9f29c8f9d07050befdc3
lilhat <tah.fighter@hotmail.co.uk>
2023-03-04 19:45:52 +0000

    best matching accuracy so far

--- webScraper_v2/tempMatcher.py
+++ webScraper_v2/tempMatcher.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import re
+
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
@@ -28,29 +30,66 @@ with open(input_file, newline='', encoding='utf-8-sig') as csvfile:
         }
         products.append(product)
 
+number_pattern = r'(\d+(?:\.\d+)?)'
 
-def fuzzy_match(product_name, row_name):
+def fuzzy_match(product_name, row_name, product_price, row_price):
     # extract the numbers from the product name and the row name
-    product_number = ''.join(filter(str.isdigit, product_name))
-    row_number = ''.join(filter(str.isdigit, row_name))
-
+    product_number = ''.join(re.findall(number_pattern, product_name))
+    row_number = ''.join(re.findall(number_pattern, row_name))
+    product_value = ''.join(re.findall(number_pattern, row_name))
+    row_value = ''.join(re.findall(number_pattern, row_name))
     # calculate the number-word weighting for matching
-    num_weight = 0.5
-    word_weight = 0.5
-
+    num_ratio = 0
+    num_weight = 0
+    word_weight = 0.8
+    price_weight = 0.2
     # calculate the partial ratio of the remaining parts of the names
-    word_ratio = fuzz.partial_ratio(product_name.replace(product_number, ''), row_name.replace(row_number, ''))
+    word_ratio = fuzz.token_set_ratio(product_name.replace(product_number, ''), row_name.replace(row_number, ''))
 
     # calculate the ratio of the numbers in the product name and the row name
     if product_number and row_number:
         num_ratio = fuzz.ratio(product_number, row_number)
+        if num_ratio == 100:
+            num_weight = 0.20
+            word_weight = 0.65
+            price_weight = 0.15
+        else:
+            word_ratio = 0
+
+    # preprocess the price values to remove non-numeric characters and convert to float
+    product_prices = re.findall(number_pattern, product_price)
+    if len(product_prices) == 2:
+        # Convert values to float and calculate average
+        product_value = (float(product_prices[0]) + float(product_prices[1])) / 2
     else:
-        num_ratio = 0
+        try:
+            product_value = float(''.join(product_prices))
+        except ValueError:
+            product_value = 0.0
+
+    row_prices = re.findall(number_pattern, row_price)
+    if len(row_prices) == 2:
+        # Convert values to float and calculate average
+        row_value = (float(row_prices[0]) + float(row_prices[1])) / 2
+    else:
+        try:
+            row_value = float(''.join(row_prices))
+        except ValueError:
+            row_value = 0.0
+
+    # calculate the percentage difference between the prices
+    if product_value and row_value:
+        price_ratio = (1 - (abs(float(product_value) - float(row_value)) / max(float(product_value), float(row_value)))) * 100
+    else:
+        price_ratio = 0
 
     # calculate the overall ratio using the number-word weighting
-    overall_ratio = (num_ratio * num_weight) + (word_ratio * word_weight)
+    num_value = (num_ratio * num_weight)
+    word_value = (word_ratio * word_weight)
+    price_value = (price_ratio * price_weight)
+    overall_ratio = num_value + word_value + price_value
 
-    return overall_ratio >= 80, overall_ratio
+    return overall_ratio >= 80, overall_ratio, num_value, word_value, price_value
 
 
 # create a dictionary to keep track of which products have already been matched in each CSV file
@@ -67,15 +106,17 @@ with open(output_file, 'w', newline='', encoding='utf-8-sig') as csvfile:
             with open(data_folder + filename, newline='', encoding='utf-8-sig') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
+                    # found_match = False
                     for product in products:
                         if product['id'] not in matched_products.get(filename, set()):
                             if product['product'] and row['product']:
                                 product_name = product['product']
                                 row_name = row['product']
-                                match_found, score = fuzzy_match(product_name, row_name)
-                                print('Comparing product:', product_name)
-                                print('To row:', row_name)
-                                print('Score:', score)
+                                product_price = product['price']
+                                row_price = row['price']
+                                product_desc = product['description']
+                                match_found, score, num, word, price = fuzzy_match(product_name, row_name, product_price, row_price)
+
                                 if match_found:
                                     match = {
                                         'id': product['id'],
@@ -87,6 +128,12 @@ with open(output_file, 'w', newline='', encoding='utf-8-sig') as csvfile:
                                         'description': row['description'],
                                         'source': filename
                                     }
+                                    print(match)
+                                    print('Comparing product:', product_name, 'Price:', product_price)
+                                    print('To row:', row_name)
+                                    print('Score:', score)
+                                    print('Num:', num, 'Word:', word, 'Price:', price)
                                     writer.writerow(match)
                                     matched_products.setdefault(filename, set()).add(product['id'])
-                                print('-----------------------------------')
+
+
