f1e73f28007406e20d46cd35df3f0c48c2b0ca5b
lilhat <tah.fighter@hotmail.co.uk>
2023-03-25 21:23:23 +0000

    launch

---
+++ public/not-found.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Not Found</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/product.css">
+    <link rel="stylesheet" href="/css/products.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ce98f0dc47.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+
+</head>   
+<body>
+    <div class="overlay"></div>
+    <nav class="navbar"></nav>
+    <div class="container">
+        
+        <div class="notfound-content-wrapper">
+            <h1 class="404-error">404 | Page Not Found</h1>
+            <h2 class="return-home">Please return back to our <a class="return-link" href="/home">home</a> page</h2>
+
+
+        </div>
+    </div>
+    <footer></footer>
+    <script type="module" src="/js/nav.js"></script>
+    <script type="module" src="/js/firebase.js"></script>
+    <script src="/js/overlay.js"></script>
+    <script src="/js/footer.js"></script>
+
+</body>
+
+</html>
